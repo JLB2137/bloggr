f346724650eacce9997b5ed4beff406d49830fe4
Eric Clatterbuck <ericclatterbuck@Erics-MacBook-Pro.local>
2022-03-10 22:53:10 -0500

    WIP: ADDED TWEET MODEL

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class Tweets(models.Model):
+    content = models.TextField(max_length=1000)
+    date_posted = models.DateTimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    likes= models.IntegerField(default=0)
+    dislikes= models.IntegerField(default=0)
+
+    def __str__(self):
+        return self.content[:5]
+
