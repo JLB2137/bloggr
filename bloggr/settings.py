621485b9f2867e0854abd21a3d382ec465a19825
Jeremee <jeremee.bornstein@gmail.com>
2022-03-12 12:18:55 -0500

    added redirects as well as signup in views, also added new url for signup for new users

--- bloggr/settings.py
+++ bloggr/settings.py
@@ -121,6 +121,9 @@ STATIC_URL = 'static/'
 #Direct the user back to the home page after login
 LOGIN_REDIRECT_URL = '/'
 
+#Direct the user back to home page after logout
+LOGOUT_REDIRECT_URL = '/'
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
--- main_app/views.py
+++ main_app/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth import login
+from django.contrib.auth.forms import UserCreationForm
 
 # Create your views here.
 
@@ -7,3 +9,12 @@ def home(request):
 
 def about(request):
     return render(request, 'about.html')
+
+def signup(request):
+    error_message = ''
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)
+            return redirect('index')
