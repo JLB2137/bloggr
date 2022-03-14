bb6d583fc60b91587a9e89e97ff032e5a22a21e7
Anton Zvontsov <82114963+azvontsov@users.noreply.github.com>
2022-03-14 16:28:14 -0400

    Revert "Anton dev"

--- main_app/urls.py
+++ main_app/urls.py
@@ -1,12 +1,6 @@
 from django.urls import path
 from . import views
 
-
-app_name = 'main_app'
-
 urlpatterns = [
-    path('',views.post_list,name="post_list"),
-    path('<slug:post>/',views.post_detail,name="post_detail"),
-    path('accounts/signup/', views.signup, name='signup'),
-    path('profile/', views.profile, name='profile'),
+    path('', views.home, name='home'),
 ]
--- main_app/views.py
+++ main_app/views.py
@@ -1,34 +1,6 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth import login
-from django.contrib.auth.forms import UserCreationForm
-from .models import Post
+from django.shortcuts import render
 
 # Create your views here.
 
-def signup(request):
-    error_message = ''
-    if request.method == 'POST':
-        form = UserCreationForm(request.POST)
-        if form.is_valid():
-            user = form.save()
-            login(request, user)
-            return redirect('home')
-        else:
-            error_message = 'Invalid signup'
-    form = UserCreationForm()
-    context = {'form': form, 'error_message': error_message,}
-    return render(request, 'registration/signup.html',context)
-
-def profile(request):
-    return render(request, 'profile.html')
-
-
-# ++++++++++++++++++++++++++++++
-
-def post_list(request):
-    posts = Post.published.all()
-    return render(request,'post_list.html',{'posts':posts})
-
-def post_detail(request, post):
-    post=get_object_or_404(Post,slug=post,status='published')
-    return render(request, 'post_detail.html',{'post':post})
+def home(request):
+    return render(request,'home.html')
