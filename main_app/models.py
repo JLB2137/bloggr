5cc3f411a73fe7b1429004e0ef9f3a1c08753946
Anton Zvontsov <azvontsov@gmail.com>
2022-03-20 10:50:28 -0400

    clean up migrations

--- main_app/models.py
+++ main_app/models.py
@@ -7,7 +7,6 @@ from taggit.managers import TaggableManager
 
 
 # Create your models here.
-
 class Tweets(models.Model):
     content = models.TextField(max_length=1000)
     date_posted = models.DateTimeField(default=timezone.now)
@@ -29,9 +28,6 @@ def get_absolute_url(self):
 
 
 
-# +++++++++++++++++++++++++++++++++++++++++++
-
-
 # creating model manager
 class PublishedManager(models.Manager):
     def get_queryset(self):
@@ -67,7 +63,6 @@ class Post(models.Model):
 
     def get_comments(self):
         return self.comments.filter(parent=None).filter(active=True)
-
 
 
 class Photo(models.Model):
@@ -76,67 +71,6 @@ class Photo(models.Model):
 
     def __str__(self):
         return f"Photo for post_id: {self.post_id} @{self.url}"
-
-
-
-class Comment(models.Model):
-    post=models.ForeignKey(Post,on_delete=models.CASCADE, related_name="comments")
-    name=models.CharField(max_length=50)
-    email=models.EmailField()
-    parent=models.ForeignKey("self", null=True, blank=True, on_delete=models.CASCADE)
-    body = models.TextField()
-    
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
-    active = models.BooleanField(default=True)
-    class Meta:
-        ordering = ('created',)
-    
-    def __str__(self):
-        return self.body
-    def get_comments(self):
-        return Comment.objects.filter(parent=self).filter(active=True)
-
-
-# +++++++++++++++++++++++++++++++++++++++++++
-
-
-# creating model manager
-class PublishedManager(models.Manager):
-    def get_queryset(self):
-        return super(PublishedManager,self).get_queryset().filter(status='published')
-
-
-# post model
-class Post(models.Model):
-    STATUS_CHOICES = (
-    ('draft', 'Draft'),
-    ('published', 'Published'),
-    )
-    title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250, unique_for_date='publish')
-    author = models.ForeignKey(User,on_delete=models.CASCADE,related_name='main_app_posts')
-    body=RichTextUploadingField()
-    publish = models.DateTimeField(default=timezone.now)
-    created = models.DateTimeField(auto_now_add=True)
-    updated = models.DateTimeField(auto_now=True)
-    tags = TaggableManager()
-    status = models.CharField(max_length=10,choices=STATUS_CHOICES,default='draft')
-    class Meta:
-        ordering = ('-publish',)
-    
-    def __str__(self):
-        return self.title
-
-    objects = models.Manager() # The default manager.
-    published = PublishedManager() # Our custom manager.
-
-    def get_absolute_url(self):
-        return reverse('main_app:post_detail',args=[self.slug])
-
-    def get_comments(self):
-        return self.comments.filter(parent=None).filter(active=True)
-
 
 
 class Comment(models.Model):
